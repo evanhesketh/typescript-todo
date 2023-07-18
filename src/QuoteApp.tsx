cf0056fed1cc73cb9e0e9e1d4e1391cd0a3db09c
Evan Hesketh <evanhesketh@yahoo.ca>
2023-07-18 15:02:43 -0400

    migrate QuoteApp to ts

---
+++ src/QuoteApp.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios, { AxiosResponse } from "axios";
+
+import QuoteDisplay from "./QuoteDisplay";
+
+/** Component that retrieves and sets an inspirational quote,
+ * shown by a child component (QuoteDisplay).
+ *
+ * Props:
+ * - None
+ *
+ * State:
+ * - [quote, setQuote]: the current quote
+ *
+ * App -> QuoteApp -> QuoteDisplay
+ */
+function QuoteApp() {
+  const [quote, setQuote] = useState<string | null>(null);
+
+  /** retrieve a random quote from quotes API */
+  async function getQuote() {
+    try {
+      const response: AxiosResponse<{ quote: string }> = await axios.get(
+        "https://inspo-quotes-api.herokuapp.com/quotes/random"
+      );
+      const randomQuote = response.data.quote;
+      setQuote(randomQuote);
+    } catch{
+      setQuote(null);
+    }
+  }
+
+  return <QuoteDisplay getQuote={getQuote} quote={quote} />;
+}
+
+export default QuoteApp;
